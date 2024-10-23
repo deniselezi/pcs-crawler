c9a60e0c25182a677666bd829136cb92a736948f
deniselezi <deniselezi2003@outlook.com>
2024-10-23 07:56:35 +0100

    implement sign in

--- crawler.py
+++ crawler.py
@@ -1,8 +1,13 @@
-from bs4 import BeautifulSoup
 from collections import deque
 
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+
 import math
-import requests
+import time
+
 
 class Crawler:
     def __init__(self, start_point, crawl_limit=None):
@@ -10,27 +15,50 @@ class Crawler:
         self.crawl_limit = crawl_limit if crawl_limit else math.inf
         self.crawls = 0
         self.mds = []
+
+        self.driver = webdriver.Chrome()  # or any other Selenium driver
+
         self.crawl()
-    
 
     def crawl(self):
         while self.to_crawl and self.crawls < self.crawl_limit:
             url = self.to_crawl.popleft()
-            print(url)
-            page = requests.get(url)
-            soup = BeautifulSoup(page.text, 'html.parser')
+            self.driver.get(url)
 
-            file_tags = soup.find_all('a', class_='Link--primary')  # list of files/dirs in repo's file explorer
-            file_tags = filter(lambda tag: len(tag.get('class')) == 1, file_tags)
+            self._sign_in()
 
-            log = set()
+    def _sign_in(self):
+        elems = self.driver.find_elements(
+            By.CLASS_NAME, "types__StyledButton-sc-ws60qy-0"
+        )
+        sign_in_button = next(filter(lambda a: a.text == "Sign in", elems))
+        
+        sign_in_button.click()
+        time.sleep(3)
 
-            for tag in file_tags:
-                if not tag.text in log:
-                    if tag.get('aria-label').find('(Directory)') != -1:
-                        self.to_crawl.append("https://github.com" + tag.get('href'))
-                        log.add(tag.text)
-                    else:  # file
-                        if tag.text.endswith('.md'):
-                            self.mds.append((tag.text, tag.get('href')))  # later edit to show location of file
-                            log.add(tag.text)
+        username_input = self.driver.find_element(
+            By.ID, "login_field"
+        )
+
+        password_input = self.driver.find_element(
+            By.ID, "password"
+        )
+
+        # print(username_input)
+        # print(password_input)
+
+        username_input.send_keys("username")
+        time.sleep(1)
+        password_input.send_keys("password")
+        time.sleep(1)
+        
+        sign_in_button = self.driver.find_element(By.XPATH, "//input[@type='submit']")
+        print(sign_in_button)
+        sign_in_button.click()
+        time.sleep(3)
+
+        # for l in login:
+        #     print(l.text)
+
+# "types__StyledButton-sc-ws60qy-0 
+# 
--- test_crawler.py
+++ test_crawler.py
@@ -1,7 +1,7 @@
 from crawler import Crawler
 
 
-search_results_url = "https://github.com/SpoonLabs/astor"
+search_results_url = "https://github.com/search?q=repo%3ASpoonLabs/astor%20path%3A.MD&type=code"
 
 crawler = Crawler(search_results_url)
 crawler.crawl()
