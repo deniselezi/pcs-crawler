d1277223935e36c726c13b57d728eeb0cb17e481
Shakir <shakirc2983@gmail.com>
2024-11-25 19:26:20 +0000

    Extracted github repos from plain-text file

--- pcs_parser/parser.py
+++ pcs_parser/parser.py
@@ -1,5 +1,7 @@
+import re
 import markdown
 from bs4 import BeautifulSoup
+
 
 class MarkdownParser:
     def __init__(self):
@@ -15,20 +17,23 @@ class MarkdownParser:
         Returns:
             the plain text
         """
-        
+
         # Convert markdown to HTML
         html_content = markdown.markdown(text)
 
         # Parse the HTML and extract plain text
-        soup = BeautifulSoup(html_content, 'html.parser')
+        soup = BeautifulSoup(html_content, "html.parser")
         text = soup.get_text()
         return text
-    
-    def extract_repos(self,link):
-        # Extract github links from plaintext file
 
-        
+    # Finds repos within the plaintext file to continue crawling
 
-        pass
+    def find_repos(self, text):
+        pattern = r"https:\/\/github\.com\/[\w\-]+\/[\w\-]+(?=\/?)"
+        return set(re.findall(pattern, text))
 
-
+    def clean(self, repos_obtained):
+        blacklist = ["https://github.com/signup/free"]
+        for blacklist_link in blacklist:
+            repos_obtained.discard(blacklist_link)
+        return repos_obtained
