57c13ede671622c56d405c1109f0c164ec5f5d73
Shakir <shakirc2983@gmail.com>
2024-12-05 17:50:20 +0000

    Created Crawler Class + Modified crawler.py

--- crawler.py
+++ crawler.py
@@ -1,6 +1,6 @@
 from collections import deque
-import math
 import sys
+import math
 import os
 
 from dotenv import find_dotenv, load_dotenv
@@ -11,52 +11,78 @@ from pcs_scraper.scraper import Scraper
 from pcs_spellchecker.spellchecker import Spellchecker
 
 
-dotenv_path = find_dotenv()
-load_dotenv(dotenv_path)
+class Crawler:
+    def __init__(self, cli, url, crawl_limit):
+        self.url = url
+        self.crawl_limit = crawl_limit
+        self.history = set()
+        self.to_crawl = deque([url])
+        self.cli = cli
+        self.scraper = Scraper(headless=False)
+        
+
+    def _get_env_variables(self, key):
+        value = os.getenv(key)
+
+        if not value:
+            self.cli.command_failed("Could not fetch {key} from .env")
+
+        return value
+
+    def start(self):
+        user = self._get_env_variables("USER")
+        password = self._get_env_variables("PASSWORD")
+
+
+        parser = MarkdownParser()
+        spellchecker = Spellchecker()
+
+        crawls = 0
+
+        while self.to_crawl and crawls < self.crawl_limit:
+            repo_misspells = (
+                {}
+            )  # dict mapping repo url to a list of misspellt words and suggested fixes
+            url = self.to_crawl.popleft()
+            if url in self.history:
+                continue
+            self.history.add(url)
+            md_contents = self.scraper.scrape(url, user, password)
+            for key, val in md_contents.items():
+                text, github_urls = parser.parse(val)
+                breakpoint()
+
+                repo_misspells[key] = spellchecker.check(
+                    text
+                )  # possible bottleneck
+                for r in github_urls:
+                    self.to_crawl.append(r)
+
+            # serialize repo_misspells somewhere over here
+
+            crawls += 1
+
+            # here, read previously serialized information and use it to generate the report
+
+        self.stop()
+
+    def stop(self):
+        self.scraper.quit()
 
 
 if __name__ == "__main__":
+    dotenv_path = find_dotenv()
+    load_dotenv(dotenv_path)
+
     cli = CLI()
     args = cli.get_args(sys.argv)
 
     URL = args[0]
-    user, password = os.getenv("USER"), os.getenv("PASSWORD")
 
     if not URL:
         cli.command_failed("Please provide an URL")
 
-    if not user or not password:
-        cli.command_failed("Could not fetch username and/or password from .env")
-
-    # start crawling
-    to_crawl = deque([URL])
-    history = set()
     CRAWL_LIMIT = math.inf if len(args) == 1 else int(args[1])
-    print(CRAWL_LIMIT)
-    parser = MarkdownParser()
-    scraper = Scraper(headless=False)
-    spellchecker = Spellchecker()
 
-    CRAWLS = 0
-    while to_crawl and CRAWLS < CRAWL_LIMIT:
-        repo_misspells = (
-            {}
-        )  # dict mapping repo url to a list of misspellt words and suggested fixes
-        url = to_crawl.popleft()
-        if url in history:
-            continue
-        history.add(url)
-        md_contents = scraper.scrape(url, user, password)
-        for key, val in md_contents.items():
-            text, github_urls = parser.parse(val)
-            repo_misspells[key] = spellchecker.check(text)  # possible bottleneck
-            for r in github_urls:
-                to_crawl.append(r)
-
-        # serialize repo_misspells somewhere over here
-
-        CRAWLS += 1
-
-    # here, read previously serialized information and use it to generate the report
-
-    scraper.quit()
+    crawler = Crawler(cli, URL, CRAWL_LIMIT)
+    crawler.start()
